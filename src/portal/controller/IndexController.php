f34c0b5f7155798b4bf5bffefe02234f14585e70
kavience <599513860@qq.com>
2023-09-12 13:37:36 +0800

    init project

---
+++ src/portal/controller/IndexController.php
@@ -0,0 +1,23 @@
+<?php
+// +----------------------------------------------------------------------
+// | RuYiCMF [ WE CAN DO IT MORE SIMPLE ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2013-present http://www.ruyicmf.com All rights reserved.
+// +----------------------------------------------------------------------
+// | Author: Dean <zxxjjforever@163.com>
+// +----------------------------------------------------------------------
+namespace api\portal\controller;
+
+use cmf\controller\RestBaseController;
+
+class IndexController extends RestBaseController
+{
+    public function index()
+    {
+        $this->success("恭喜您,API访问成功!", [
+            'version' => '1.1.0',
+            'doc'     => 'http://www.ruyicmf.com/cmf5api.html'
+        ]);
+    }
+
+}
